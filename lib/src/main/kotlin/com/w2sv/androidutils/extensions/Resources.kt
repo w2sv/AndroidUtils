2380f0548ecd8a3e0e9eef393571a08ad6477a6e
w2sv <zangenbergjanek@googlemail.com>
2023-01-21 08:51:05 +0100

    Update kotlin & AGP; Add Resources.getHtmlText

--- lib/src/main/kotlin/com/w2sv/androidutils/extensions/Resources.kt
+++ lib/src/main/kotlin/com/w2sv/androidutils/extensions/Resources.kt
@@ -3,7 +3,19 @@
 package com.w2sv.androidutils.extensions
 
 import android.content.res.Resources
+import android.text.Spanned
 import androidx.annotation.IntegerRes
+import androidx.annotation.StringRes
+import androidx.core.text.HtmlCompat
 
 fun Resources.getLong(@IntegerRes id: Int): Long =
     getInteger(id).toLong()
+
+fun Resources.getHtmlText(@StringRes id: Int, vararg formatArgs: Any): Spanned =
+    HtmlCompat.fromHtml(
+        getString(
+            id,
+            *formatArgs
+        ),
+        HtmlCompat.FROM_HTML_MODE_COMPACT
+    )
