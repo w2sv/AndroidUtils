35d6a00208a89232d192a917e3dc22ec87bb757a
w2sv <zangenbergjanek@googlemail.com>
2023-05-05 12:55:06 +0200

    Add Context.openUrlWithActivityNotFoundHandling

--- lib/src/main/kotlin/com/w2sv/androidutils/generic/ActivityLaunching.kt
+++ lib/src/main/kotlin/com/w2sv/androidutils/generic/ActivityLaunching.kt
@@ -2,10 +2,14 @@
 
 package com.w2sv.androidutils.generic
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
+import com.w2sv.androidutils.R
+import com.w2sv.androidutils.notifying.showToast
 
 fun Context.openUrl(url: String) {
     startActivity(
@@ -15,6 +19,22 @@ fun Context.openUrl(url: String) {
         )
             .addCategory(Intent.CATEGORY_BROWSABLE)
     )
+}
+
+fun Context.openUrlWithActivityNotFoundHandling(
+    url: String,
+    onActivityNotFoundException: (Context) -> Unit = {
+        showToast(
+            getString(R.string.couldn_t_find_a_browser_to_open_the_url_with),
+            Toast.LENGTH_LONG
+        )
+    }
+) {
+    try {
+        openUrl(url)
+    } catch (e: ActivityNotFoundException) {
+        onActivityNotFoundException(this)
+    }
 }
 
 val ActivityResult.uris: List<Uri>?
