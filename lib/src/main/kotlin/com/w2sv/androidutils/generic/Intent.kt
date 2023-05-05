8157ccd55c2e7fa09cab3490a8117cc528b69455
w2sv <zangenbergjanek@googlemail.com>
2023-05-05 13:19:46 +0200

    Add Intent & Bundle parcelable retrieval convenience extension functions

--- lib/src/main/kotlin/com/w2sv/androidutils/generic/Intent.kt
+++ lib/src/main/kotlin/com/w2sv/androidutils/generic/Intent.kt
@@ -3,7 +3,22 @@
 package com.w2sv.androidutils.generic
 
 import android.content.Intent
+import android.os.Parcelable
+import androidx.core.content.IntentCompat
 
+inline fun <reified T : Parcelable> Intent.getParcelableCompat(name: String): T? =
+    IntentCompat.getParcelableExtra(this, name, T::class.java)
+
+inline fun <reified T : Parcelable> Intent.getParcelableArrayListCompat(name: String): ArrayList<T>? =
+    IntentCompat.getParcelableArrayListExtra(this, name, T::class.java)
+
+inline fun <reified T : Parcelable> Intent.getParcelableArrayCompat(name: String): Array<out Parcelable>? =
+    IntentCompat.getParcelableArrayExtra(this, name, T::class.java)
+
+/**
+ * Returns null instead of [defaultValue] for more convenient handling through
+ * built-in null handling mechanisms.
+ */
 fun Intent.getIntExtraOrNull(name: String, defaultValue: Int): Int? =
     getIntExtra(name, defaultValue).run {
         if (equals(defaultValue))
