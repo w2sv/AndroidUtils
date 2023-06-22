7627e6bb4dffc4834e6ae1a4fe0b4e949b1535f0
w2sv <zangenbergjanek@googlemail.com>
2023-06-22 23:16:20 +0200

    Revise DataStoreEntry

--- lib/src/main/kotlin/com/w2sv/androidutils/datastorage/datastore/preferences/DataStoreEntry.kt
+++ lib/src/main/kotlin/com/w2sv/androidutils/datastorage/datastore/preferences/DataStoreEntry.kt
@@ -7,12 +7,27 @@ import androidx.datastore.preferences.core.Preferences
  * Interface for Classes, which are to be managed as map by a [AbstractPreferencesDataStoreRepository].
  */
 interface DataStoreEntry<T, KeyType> {
-    val defaultValue: T
     val preferencesKey: Preferences.Key<KeyType>
+    val defaultValue: T
 
-    interface UniType<T> : DataStoreEntry<T, T>
+    interface UniType<T> : DataStoreEntry<T, T>{
+        abstract class Impl<T>(
+            override val preferencesKey: Preferences.Key<T>,
+            override val defaultValue: T
+        ): UniType<T>
+    }
 
-    interface EnumValued<E : Enum<E>> : DataStoreEntry<E, Int>
+    interface EnumValued<E : Enum<E>> : DataStoreEntry<E, Int> {
+        abstract class Impl<E : Enum<E>>(
+            override val preferencesKey: Preferences.Key<Int>,
+            override val defaultValue: E
+        ) : EnumValued<E>
+    }
 
-    interface UriValued : DataStoreEntry<Uri?, String>
+    interface UriValued : DataStoreEntry<Uri?, String>{
+        abstract class Impl(
+            override val preferencesKey: Preferences.Key<String>,
+            override val defaultValue: Uri?
+        ): UriValued
+    }
 }
