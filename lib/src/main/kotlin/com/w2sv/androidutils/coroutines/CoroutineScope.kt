f9ac297fb8e7f73cc1d4272feda3d6fc803d60a5
w2sv <zangenbergjanek@googlemail.com>
2024-04-19 10:14:01 +0200

    Add CoroutineScope.collectLatestFromFlow

--- lib/src/main/kotlin/com/w2sv/androidutils/coroutines/CoroutineScope.kt
+++ lib/src/main/kotlin/com/w2sv/androidutils/coroutines/CoroutineScope.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
@@ -31,4 +32,14 @@ fun <T> CoroutineScope.collectFromFlow(
 ): Job =
     launch(context, start) {
         flow.collect(flowCollector)
+    }
+
+fun <T> CoroutineScope.collectLatestFromFlow(
+    flow: Flow<T>,
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    action: suspend (value: T) -> Unit
+): Job =
+    launch(context, start) {
+        flow.collectLatest(action)
     }
