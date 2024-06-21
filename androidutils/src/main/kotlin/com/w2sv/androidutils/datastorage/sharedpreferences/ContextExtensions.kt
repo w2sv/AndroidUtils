ad07a46c99a90b261192bdffad74ee51435bc062
w2sv <zangenbergjanek@googlemail.com>
2024-06-21 14:14:03 +0200

    Remove typedpreferences

--- androidutils/src/main/kotlin/com/w2sv/androidutils/datastorage/sharedpreferences/ContextExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.w2sv.androidutils.datastorage.sharedpreferences
-
-import android.content.Context
-import android.content.SharedPreferences
-
-fun Context.getSharedPreferences(name: String = packageName): SharedPreferences =
-    getSharedPreferences(name, Context.MODE_PRIVATE)
