21c357d4f2d8e6712ac8b7b38202d7d7b7881e7b
w2sv <zangenbergjanek@googlemail.com>
2024-11-16 18:46:02 +0100

    Rename Resources.getText to Resources.getHtmlFormattedText & improve KDoc

--- androidutils/core/src/main/kotlin/com/w2sv/androidutils/res/Resources.kt
+++ androidutils/core/src/main/kotlin/com/w2sv/androidutils/res/Resources.kt
@@ -14,28 +14,30 @@ fun Resources.getLong(@IntegerRes id: Int): Long =
     getInteger(id).toLong()
 
 /**
- * Create a formatted CharSequence from a string resource containing arguments and HTML formatting.
+ * Creates a [CharSequence] from a string resource, which may contain arguments and HTML formatting.
  *
- * The string resource must be wrapped in a CDATA section so that the HTML formatting is conserved.
+ * The string resource must be wrapped in a __CDATA__ section so that the HTML formatting is preserved, like for instance so:
+ * ```
+ * <string name="html_formatted"><![CDATA[bold text: <b>%1$s</b>]]></string>
+ * ```
  *
- * Example of an HTML formatted string resource:
- * <string name="html_formatted"><![CDATA[ bold text: <B>%1$s</B> ]]></string>
- *
- * Taken from https://stackoverflow.com/a/56944152/12083276
+ * Taken from [here](https://stackoverflow.com/a/56944152/12083276)
  */
-fun Resources.getText(@StringRes id: Int, vararg args: Any?): CharSequence =
+fun Resources.getHtmlFormattedText(@StringRes id: Int, vararg args: Any?): CharSequence =
     HtmlCompat.fromHtml(getString(id, *args), HtmlCompat.FROM_HTML_MODE_COMPACT)
 
 /**
- * Create a formatted CharSequence from a plurals resource containing arguments and HTML formatting.
+ * Creates a formatted [CharSequence] from a plurals resource, which may contain arguments and HTML formatting.
  *
- * The plurals resource must be wrapped in a CDATA section so that the HTML formatting is conserved.
+ * The plurals resource must be wrapped in a __CDATA__ section so that the HTML formatting is conserved.
  *
  * Example of an HTML formatted plurals resource:
+ * ```
  * <plurals name="html_formatted">
- *         <item quantity="one"><![CDATA[bold text: <B>%1$s</B>]]></item>
- *         <item quantity="other"><![CDATA[bold texts: <B>%1$s</B>]]></item>
- *     </plurals>
+ *      <item quantity="one"><![CDATA[bold text: <B>%1$s</B>]]></item>
+ *      <item quantity="other"><![CDATA[bold texts: <B>%1$s</B>]]></item>
+ * </plurals>
+ * ```
  */
 fun Resources.getQuantityText(
     @PluralsRes id: Int,
