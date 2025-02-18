0201a963380dd81ce68347e8872107d91a617136
w2sv <zangenbergjanek@googlemail.com>
2025-02-18 12:06:40 +0100

    Update gradle to 8.11.1 | Format libs.versions.toml

--- androidutils/lifecycle/src/test/kotlin/com/w2sv/androidutils/lifecycle/LiveDataKtTest.kt
+++ androidutils/lifecycle/src/test/kotlin/com/w2sv/androidutils/lifecycle/LiveDataKtTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(InstantExecutorExtension::class)
 internal class LiveDataKtTest {
+
     @Test
     fun toggleBooleanLiveData() {
         with(MutableLiveData(true)) {
