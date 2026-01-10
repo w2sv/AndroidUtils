15a6a0f933f0c8ba44010252d8d4e4d7100d573c
w2sv <zangenbergjanek@googlemail.com>
2026-01-11 00:41:32 +0100

    feat: add intent convenience functions

--- androidutils/core/src/main/kotlin/com/w2sv/androidutils/content/Intent.kt
+++ androidutils/core/src/main/kotlin/com/w2sv/androidutils/content/Intent.kt
@@ -1,10 +1,27 @@
 @file:Suppress("unused")
 
-package com.w2sv.androidutils.os
+package com.w2sv.androidutils.content
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.os.Parcelable
 import androidx.core.content.IntentCompat
+import com.w2sv.androidutils.os.toMapString
+
+inline fun <reified T> intent(context: Context): Intent =
+    Intent(context, T::class.java)
+
+/**
+ * @see [Intent.makeRestartActivityTask]
+ */
+inline fun <reified T> restartActivityTaskIntent(context: Context): Intent =
+    Intent.makeRestartActivityTask(
+        ComponentName(
+            context,
+            T::class.java
+        )
+    )
 
 inline fun <reified T : Parcelable> Intent.getParcelableCompat(name: String): T? =
     IntentCompat.getParcelableExtra(this, name, T::class.java)
