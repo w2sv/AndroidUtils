8b738b838bf7b354a971e6542b864f8105a40b04
w2sv <zangenbergjanek@googlemail.com>
2026-01-18 21:13:53 +0100

    build: do not set version from version.txt, since there is no need for it

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,3 @@ plugins {
 versionCatalogUpdate {
     versionSelector(VersionSelectors.PREFER_STABLE)
 }
-
-// Set version from $ROOT/version.txt, which is created or updated when running the make publish routine
-rootProject.layout.projectDirectory.file("version.txt").asFile.let { versionFile ->
-    if (versionFile.exists()) {
-        version = versionFile.readText().trim()
-    }
-}
