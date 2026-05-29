17a472b37740f889afb27e6fe38b7533dbdae7c2
w2sv <zangenbergjanek@googlemail.com>
2026-05-29 17:18:40 +0200

    chore: update dependencies & disable unnecessary build features

--- androidutils/core/build.gradle.kts
+++ androidutils/core/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat.resources)
-    implementation(libs.w2sv.kotlinutils)
+    implementation(libs.w2sv.kotlinutils.coroutines)
 
     testImplementation(libs.bundles.unitTest)
 }
